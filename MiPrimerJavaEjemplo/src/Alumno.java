f0e5ebf12d1f1368f1e3e40982fcc9b5abd3a817
Francesca Carolina Condor Llontop <a20176818@pucp.edu.pe>
2026-07-01 21:35:32 -0500

    JAVA

---
+++ MiPrimerJavaEjemplo/src/Alumno.java
@@ -0,0 +1,30 @@
+
+public class Alumno extends Persona {
+    private int codigo;
+    private  double craest;
+    private  String correo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getCraest() {
+        return craest;
+    }
+
+    public void setCraest(double craest) {
+        this.craest = craest;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
